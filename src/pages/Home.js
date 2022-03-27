8b5946b6837ee68325b956a862bb11f2fc463e72
unknown <3933714051@bitraipur.ac.in>
2022-03-27 19:35:01 +0530

    added prop validation to check props and navbar component and its styles and added this to the app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,58 +1,66 @@
+import PropTypes from 'prop-types';
+
 import styles from '../styles/home.module.css';
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <div className={styles.postsList}>
-      <div className={styles.postWrapper}>
-        <div className={styles.postHeader}>
-          <div className={styles.postAvatar}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/4333/4333609.png"
-              alt="user-pic"
-            />
-            <div>
-              <span className={styles.postAuthor}>shubham</span>
-              <span className={styles.postTime}>a minute ago</span>
-            </div>
-          </div>
-          <div className={styles.postContent}>Post Conetnt</div>
-
-          <div className={styles.postActions}>
-            <div className={styles.postLike}>
+      {posts.map((post) => (
+        <div className={styles.postWrapper} key={`post-${post._id}`}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
               <img
-                src="https://cdn-icons-png.flaticon.com/512/833/833472.png"
-                alt="likes-icon"
+                src="https://cdn-icons-png.flaticon.com/512/4333/4333609.png"
+                alt="user-pic"
               />
-              <span>5</span>
+              <div>
+                <span className={styles.postAuthor}>{post.user.name}</span>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
             </div>
+            <div className={styles.postContent}>{post.content}</div>
 
-            <div className={styles.postCommentsIcon}>
-              <img
-                src="https://cdn-icons.flaticon.com/png/512/2593/premium/2593491.png?token=exp=1648365493~hmac=8cc8da6dbfbc3872e8500e046e1c4f1a"
-                alt="comments-icon"
-              />
-              <span>2</span>
-            </div>
-          </div>
-          <div className={styles.postCommentBox}>
-            <input placeholder="Start typing a comment" />
-          </div>
-
-          <div className={styles.postCommentsList}>
-            <div className={styles.postCommentsItem}>
-              <div className={styles.postCommentHeader}>
-                <span className={styles.postCommentAuthor}>Bill</span>
-                <span className={styles.postCommentTime}>a minute ago</span>
-                <span className={styles.postCommentLikes}>22</span>
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/833/833472.png"
+                  alt="likes-icon"
+                />
+                <span>5</span>
               </div>
 
-              <div className={styles.postCommentContent}>Random comment</div>
+              <div className={styles.postCommentsIcon}>
+                <img
+                  src="https://cdn-icons.flaticon.com/png/512/2593/premium/2593491.png?token=exp=1648365493~hmac=8cc8da6dbfbc3872e8500e046e1c4f1a"
+                  alt="comments-icon"
+                />
+                <span>2</span>
+              </div>
+            </div>
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
+            </div>
+
+            <div className={styles.postCommentsList}>
+              <div className={styles.postCommentsItem}>
+                <div className={styles.postCommentHeader}>
+                  <span className={styles.postCommentAuthor}>Bill</span>
+                  <span className={styles.postCommentTime}>a minute ago</span>
+                  <span className={styles.postCommentLikes}>22</span>
+                </div>
+
+                <div className={styles.postCommentContent}>Random comment</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
+};
+
+Home.propTypes = {
+  posts: PropTypes.array.isRequired,
 };
 
 export default Home;
