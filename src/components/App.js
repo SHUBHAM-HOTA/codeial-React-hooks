0da248cff6c8640cfb110bfb2dd5a74e16f79f17
unknown <3933714051@bitraipur.ac.in>
2022-04-02 20:49:37 +0530

    made some correction in the routes in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { getPosts } from '../api';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -24,10 +38,22 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Navbar />
-      <Home posts={posts} />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home posts={posts} />} />
+
+          <Route exact path="/login" element={<Login />} />
+
+          <Route exact path="/About" element={<About />} />
+
+          <Route exact path="/user/asdasd" element={<UserInfo />} />
+
+          <Route element={<Page404 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
