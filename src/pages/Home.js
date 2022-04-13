4fa81af8d0127e567d68306c63b1d35444bc058c
unknown <3933714051@bitraipur.ac.in>
2022-04-13 10:48:20 +0530

    added the add comment feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 // import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Comment, FriendsList, Loader, CreatePost } from '../components';
+import { Comment, FriendsList, Loader, CreatePost, Post } from '../components';
 // import { getPosts } from '../api';
 import styles from '../styles/home.module.css';
 import { useAuth, usePosts } from '../hooks';
@@ -40,58 +40,7 @@ const Home = () => {
       <div className={styles.postsList}>
         <CreatePost />
         {posts.data.map((post) => (
-          <div className={styles.postWrapper} key={`post-${post._id}`}>
-            <div className={styles.postHeader}>
-              <div className={styles.postAvatar}>
-                <img
-                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                  alt="user-pic"
-                />
-                <div>
-                  <Link
-                    to={{
-                      pathname: `/user/${post.user._id}`,
-                      // state: {
-                      //   user: post.user,
-                      // },
-                    }}
-                    className={styles.postAuthor}
-                  >
-                    {post.user.name}
-                  </Link>
-                  <span className={styles.postTime}>a minute ago</span>
-                </div>
-              </div>
-              <div className={styles.postContent}>{post.content}</div>
-
-              <div className={styles.postActions}>
-                <div className={styles.postLike}>
-                  <img
-                    src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
-                    alt="likes-icon"
-                  />
-                  <span>{post.likes.length}</span>
-                </div>
-
-                <div className={styles.postCommentsIcon}>
-                  <img
-                    src="https://image.flaticon.com/icons/svg/1380/1380338.svg"
-                    alt="comments-icon"
-                  />
-                  <span>{post.comments.length}</span>
-                </div>
-              </div>
-              <div className={styles.postCommentBox}>
-                <input placeholder="Start typing a comment" />
-              </div>
-
-              <div className={styles.postCommentsList}>
-                {post.comments.map((comment) => (
-                  <Comment comment={comment} />
-                ))}
-              </div>
-            </div>
-          </div>
+          <Post post={post} key={`post-${post._id}`} />
         ))}
       </div>
       {auth.user && <FriendsList />}
